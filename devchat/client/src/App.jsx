f90da791953ae43f6f3ad609ef1d37e841e6542e
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-04-23 10:39:34 -0300

    feat: configuração inicial do React com Vite, estilos e componente Join
    Adiciona estrutura base da aplicação DevChat com:
    - Setup Vite + React
    - Componente Join para entrada na sala de chat
    - Estilos CSS com variáveis de tema
    - Dependências (Socket.io, Material-UI, Emotion)
    - Configuração do servidor de desenvolvimento

---
+++ devchat/client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Join from "./components/Join/join";
+
+function App() {
+  return (
+    <>
+      <Join />
+    </>
+  );
+}
+
+export default App;
---
+++ devchat/client/src/components/Join/Join.jsx
@@ -0,0 +1,11 @@
+import style from "./Join.module.css";
+
+const Join = () => {
+  return (
+  <div>Join</div>
+);
+};
+
+export default Join;
+
+
