507995940b7a4149dbb281b83cc731b41199d147
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-04-23 15:56:11 -0300

    feat: adicionar estilo ao componente de chat e implementar logo do DevChat

--- devchat/client/src/components/Join/Join.jsx
+++ devchat/client/src/components/Join/Join.jsx
@@ -4,6 +4,8 @@ import style from "./Join.module.css";
 import { Input, Button } from "@mui/material";
 import io from "socket.io-client";
 
+import logo from "../../assets/devChat.png";
+
 const Join = (props) => {
   const usernameRef = useRef();
 
@@ -26,7 +28,7 @@ const Join = (props) => {
   return (
     <>
       <div className={style.dev_logo}>
-        <img src="" alt="Logo do DevChat" />
+        <img src={logo} alt="Logo do DevChat" />
       </div>
 
       <div className={style.join_container}>
